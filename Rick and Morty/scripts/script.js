b6f2b8908484ebbc370b584dc59c268ede4f78aa
alissa <alissanishihara@gmail.com>
2022-04-17 16:17:09 +0200

    done main area

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -1,61 +1,99 @@
-//query all the episodes
+//Funcao que rendeiza todos os episodios
 
-async function fetchCharacters(characterURLs) {
-    const charactersFetchPrimoses = characterURLs.map(characterURL => fetch(characterURL));
-    const resolvedFetchResponses = resolvedFetchResponses.map(resolvedFetchResponses => resolvedFetchResponses.json())
-}
-
-function updateMainArea(name, date, episodeCode, characters) {
-    mainAreaElement.innerHtml = "";
-
-    const title= document.createElement("h2");
-    titleElement.innerText = name;
+function updateMainArea(episode) {
+    const titleElement = document.createElement("h2");
+    titleElement.innerText = `${episode.name}`;
     const dateAndCodeElement = document.createElement("h3");
-    dateAndCodeElement.innerText = `${date} - ${episodeCode}`
+    dateAndCodeElement.innerText = `${episode.air_date} | ${episode.episode}`
 
-    mainAreaElement.appendChild(titleElement)
-    mainArea.appendChild(dateAndCodeElement)
+   let mainAreaElement = document.querySelector("#main-area");
+   mainAreaElement.innerHTML = "";
+   mainAreaElement.appendChild(titleElement);
+   mainAreaElement.appendChild(dateAndCodeElement);
+   console.log(episode)
+   
+   episode.characters.forEach(character => {
 
-    fetchCharacters(characterURLs);
+       fetch(character)
+         .then(result => {
+             return result.json()
+         })
+         .then(result => {
+            let characterElement = document.createElement("div");
+            let imageElement = document.createElement("img");
+            imageElement.src = result.image;
+            characterElement.appendChild(imageElement);
+            mainAreaElement.appendChild(characterElement);
+            const createPname = document.createElement("p");
+            createPname.innerText = `${result.name}`;
+            characterElement.appendChild(createPname);
+            const createPdescription = document.createElement("p");
+            createPdescription.innerText = `${result.species} | ${result.status}`;
+            characterElement.appendChild(createPdescription);
 
+         })
 
-
-}
-
-
-
-
-function sidebar() {
-    fetch("https://rickandmortyapi.com/api/episode")
-     .then(result => {
-        return result.json()
-     .then(json => {
-        const sideBarElement = document.createElement("div");
-        sideBarElement.id = "sidebar";
-        document.querySelector("#root").appendChild(sideBarElement);
-        console.log(json.results);
-        json.results.forEach(episode => {
-            console.log(`Episode ${episode.id}`)
-            const titleElement = document.createElement("p");
-            titleElement.innerText = `Episode ${titleElement}`;
-            sideBarElement.appendChild(titleElement);
-            titleElement.addEventListener("click", event => {
-                console.log(episode.name);
-            })
-         
-        });
-        const firstEpisode = json.results[0];
-        updateMainArea(episode.name, episode.airDate, episode.episode, episode.characters);
-    });
-    });
-
-function mainArea() {
-    const mainAreaElement = document.createElement("div");
-    document.querySelector("#root").appendChild(mainAreaElement);
-    mainArea.innerText = "This is my main area"
+   })
 };
 
 
-sidebar();
+
+
+function mainArea() {
+    const mainElement = document.createElement('div');
+    mainElement.className = 'main-area';
+    mainElement.id = "main-area"
+    document.querySelector("#root").appendChild(mainElement);
+    mainElement.innerHTML = 'main area'
+}
+
+function createTitle() {
+    const titleElement = document.createElement("div");
+    titleElement.className = "title"
+    document.querySelector("#root").appendChild(titleElement);
+    titleElement.innerHTML = "<strong>Rick y Morty API</strong>"
+}
+
+
+
+function sideBar() {
+    fetch("https://rickandmortyapi.com/api/episode")
+        .then(result => {
+            return result.json()
+        })
+        .then(json => {
+            const sideBarElement = document.createElement("div");
+            sideBarElement.id = "sidebar";
+            document.querySelector("#root").appendChild(sideBarElement);
+            console.log(json.results);
+            const linkList = document.createElement("ul");
+            sideBarElement.appendChild(linkList);
+
+            json.results.forEach(episode => {
+                console.log(`Episode ${episode.id}`)
+
+                const titleElement = document.createElement("a");
+                const linkText = document.createTextNode(`Episode ${episode.id}`);
+                titleElement.appendChild(linkText);
+                titleElement.href = '#';
+                
+
+                const LinkLi = document.createElement("li");
+                linkList.appendChild(LinkLi);
+
+                LinkLi.appendChild(titleElement);
+                
+                titleElement.addEventListener("click", event => {
+                    updateMainArea(episode);
+                    console.log(episode.name);
+                    event.preventDefault();
+                });
+            });
+        });
+}
+
+
+createTitle();
+sideBar();
 mainArea();
 
